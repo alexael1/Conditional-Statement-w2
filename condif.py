cb926fd9adf7255e3e1998f7fa38e8f40832a249
alexael1 <alexphro@proton.me>
2026-02-28 12:17:13 +0000

    test1

---
+++ condif.py
@@ -0,0 +1,10 @@
+name = input("Enter your name:")
+if name == "Alex":
+    print("Correct")
+elif name =="John":
+    print("Correct")
+elif name=="Piotr":
+    print("Correct")
+    
+else:
+    print("Try again")
