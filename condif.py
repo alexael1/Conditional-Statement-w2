8d500514305d0e5a928a3e2db8521121af1f5f5c
alexael1 <alexphro@proton.me>
2026-02-28 12:36:11 +0000

    update1

--- condif.py
+++ condif.py
@@ -3,3 +3,12 @@ bio= int(input("Enter bio marks "))
 db= int(input("Enter db marks "))
 total= math + bio + db
 avg= total/3 
+print("Total marks is ", total, "and average is ", avg)
+if avg < 40 and avg >= 0:
+    print("Fail")
+elif avg >=40 and avg <=50:
+    print("Pass")
+elif avg > 50 and avg <=60:
+    print("Merit")
+elif avg > 60 and avg <=70:
+    print("Dist")
